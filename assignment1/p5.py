a6de35e2168041a460d08589f96ff9e5b6c11778
Noel <kxwhi@connect.hku.hk>
2024-02-01 21:39:48 +0800

    Finish p5

--- assignment1/p5.py
+++ assignment1/p5.py
@@ -1,9 +1,33 @@
 import sys, parse, grader
+from heapq import heappush, heappop
+
 
 def astar_search(problem):
-    #Your p5 code here
-    solution = 'S D C B\nS C G'
+    start = problem['start']
+    goal = problem['goal']
+    frontier = []
+    heappush(frontier, (problem[start][0], [start, ]))
+    # in frontier, node: (distance, [list of nodes in the path])
+    path = []
+    exploredList = list()
+    while frontier:
+        node = heappop(frontier)
+        if node[-1][-1] == goal:
+            path = node[-1]
+            break
+        if node[-1][-1] not in exploredList:
+            exploredList.append(node[-1][-1])
+            for child in problem[node[1][-1]]:
+                if type(child) is tuple:
+                    newSequence = [i for i in node[1]]
+                    newSequence.append(child[0])
+                    originH = problem[newSequence[-2]][0] # the origin heuristic
+                    heappush(frontier, (node[0] - originH + child[1] + problem[child[0]][0], newSequence))
+
+    deli = " "
+    solution = deli.join(exploredList) + '\n' + deli.join(path)
     return solution
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
