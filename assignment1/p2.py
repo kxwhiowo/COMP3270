f97acca7ea21e7a04d6156f558a65d5949fe286f
kxwhi <kxwhi@connect.hku.hk>
2024-02-01 16:12:23 +0800

    Finish p2

--- assignment1/p2.py
+++ assignment1/p2.py
@@ -1,9 +1,32 @@
-import sys, grader, parse
+import sys, grader, parse, collections
+
 
 def bfs_search(problem):
-    #Your p2 code here
-    solution = 'Ar B C D\nAr C G'
+    # print(problem)
+    path = []
+    start = problem['start']
+    goal = problem['goal']
+    frontier = collections.deque([[start, ]])
+    exploredList = list()
+    while frontier:
+        node = frontier.popleft()
+        if node[-1] == goal:
+            path = node
+            break
+        if node[-1] not in exploredList:
+            # print('Explore: ' + node[-1])
+            exploredList.append(node[-1])
+            # print(problem[node[-1]])
+            for child in problem[node[-1]]:
+                if type(child) is tuple:
+                    newNode = [i for i in node]
+                    newNode.append(child[0])
+                    frontier.append(newNode)
+    deli = " "
+    solution = deli.join(exploredList) + '\n' + deli.join(path)
+    # solution = 'Ar B C D\nAr C G'
     return solution
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
