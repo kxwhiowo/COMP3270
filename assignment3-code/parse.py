b190c5b4d9e1fb2ebffa44c4cfd3b6060bec28eb
kxwhi <kxwhi@connect.hku.hk>
2024-04-02 17:07:05 +0800

    Almost finish p1

---
+++ assignment3-code/parse.py
@@ -0,0 +1,22 @@
+def read_grid_mdp_problem_p1(file_path):
+    problem = ''
+    with open(file_path) as f:
+        lines = f.readlines()
+        for i in lines:
+            problem += i
+    return problem
+
+def read_grid_mdp_problem_p2(file_path):
+    problem = ''
+    with open(file_path) as f:
+        lines = f.readlines()
+        for i in lines:
+            problem += i
+    return problem
+
+def read_grid_mdp_problem_p3(file_path):
+    #Your p3 code here
+    problem = ''
+    return problem
+
+#print(read_grid_mdp_problem_p1("test_cases/p1/1.prob"))
